e549b2605aee8c4b67b6a3f860492bf2f3a495f1
Cédric Rochefolle <85271877+IndyTheDog@users.noreply.github.com>
2023-04-24 16:43:35 +0700

    Attempt to dynamically assign SonarCloud project properties #5

--- etc/bin/sonar.sh
+++ etc/bin/sonar.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sed -i 's/SONAR_PROJECT_KEY/IndyTheDog_jsc-bot-detection/' build.gradle.kts
-sed -i 's/SONAR_ORGANISATION/indythedog/' build.gradle.kts
-sed -i 's/SONAR_HOST/https:\/\/sonarcloud.io/' build.gradle.kts
-./gradlew build sonarqube --info
+SONAR_PROJECT_KEY=${env:GITHUB_REPOSITORY_OWNER}
+SONAR_ORGANISATION=${env:GITHUB_REPOSITORY_OWNER,,}
+
+sed -i "s/SONAR_PROJECT_KEY/${SONAR_PROJECT_KEY}_jsc-bot-detection/" build.gradle.kts
+sed -i "s/SONAR_ORGANISATION/${SONAR_ORGANISATION}/" build.gradle.kts
+sed -i "s/SONAR_HOST/https:\/\/sonarcloud.io/" build.gradle.kts
+
+cat build.gradle.kts
+
+# ./gradlew build sonarqube --info
