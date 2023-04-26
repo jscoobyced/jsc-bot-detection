e812ae2eb83d1f930fb67ddc0f32b6486bb1ae58
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2023-04-26 20:50:24 +0700

    Fix code-smell reported by SonarCloud.

--- src/main/kotlin/io/narok/services/DeviceInformationService.kt
+++ src/main/kotlin/io/narok/services/DeviceInformationService.kt
@@ -2,12 +2,16 @@ package io.narok.services
 
 import io.narok.models.DeviceInformation
 import io.narok.models.DeviceInformationRequest
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 
-class DeviceInformationService(override val di: DI) : IDeviceInformationService, DIAware {
+class DeviceInformationService(
+    override val di: DI,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Default
+) : IDeviceInformationService, DIAware {
     private val deviceSignatureService = DeviceSignatureService()
     private val deviceTypeService = DeviceTypeService(di)
 
@@ -21,11 +25,11 @@ class DeviceInformationService(override val di: DI) : IDeviceInformationService,
             .withSignature(deviceInformationWithSignature.deviceSignature)
     }
 
-    private suspend fun createSignature(deviceInformation: DeviceInformation) = withContext(Dispatchers.Default) {
+    private suspend fun createSignature(deviceInformation: DeviceInformation) = withContext(dispatcher) {
         deviceSignatureService.createSignature(deviceInformation)
     }
 
-    private suspend fun createDeviceType(deviceInformation: DeviceInformation) = withContext(Dispatchers.Default) {
+    private suspend fun createDeviceType(deviceInformation: DeviceInformation) = withContext(dispatcher) {
         deviceTypeService.createDeviceType(deviceInformation)
     }
 
